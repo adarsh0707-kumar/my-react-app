212c30e86c38d461adccb99788b25c91cc0539f5
Adarsh Kumar <adarshku123456789gmail.com>
2025-06-12 01:40:55 +0530

    First

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <h1
+        className="font-bold text-5xl text-blue-500">
+        Adarsh
+      </h1>
+    </>
+  )
+}
+
+export default App
